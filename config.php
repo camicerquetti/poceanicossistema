f5e5d8a19e5a576f90b875686896f2738efe0caf
camicerquetti <104858176+camicerquetti@users.noreply.github.com>
2025-04-04 21:15:50 -0300

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 // Configuración de la base de datos
-$servidor = "localhost"; // Nombre del servidor de la base de datos
-$usuario = "root";       // Nombre de usuario de la base de datos
-$password = "";         // Contraseña de la base de datos (deja vacío si no tiene)
-$basededatos = "productosoceanicos"; // Reemplaza con el nombre de tu base de datos
+$servidor = "botcdmg86r1dbzg5mpyr-mysql.services.clever-cloud.com"; // Nombre del servidor de la base de datos
+$usuario = "upztvkwubpdw87yq";       // Nombre de usuario de la base de datos
+$password = "upztvkwubpdw87yq";         // Contraseña de la base de datos (deja vacío si no tiene)
+$basededatos = "botcdmg86r1dbzg5mpyr"; // Reemplaza con el nombre de tu base de datos
 
 // Crear conexión
 $conn = new mysqli($servidor, $usuario, $password, $basededatos);
